95d3cc3c5646e7cd5a4b437266c8fc19495b33e8
Arrow <aronvinicius15@gmail.com>
2021-04-19 02:22:31 -0300

    adding dbsets and mapping to context file

--- StockSystem.Infra.Data/Context/ApplicationDbContextAPI.cs
+++ StockSystem.Infra.Data/Context/ApplicationDbContextAPI.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using StockSystem.Domain.Entities;
+using StockSystem.Infra.Data.Mappings;
 
 namespace StockSystem.Infra.Data.Context
 {
@@ -8,8 +10,28 @@ namespace StockSystem.Infra.Data.Context
             : base(options)
         { }
 
+        public DbSet<DescriptionRawMaterial> DescriptionRawMaterial { get; set; }
+        public DbSet<Establishment> Establishment { get; set; }
+        public DbSet<HistoryRawMaterial> HistoryRawMaterial { get; set; }
+        public DbSet<RawMaterial> RawMaterial { get; set; }
+        public DbSet<SaleDay> SaleDay { get; set; }
+        public DbSet<StatisticsDay> StatisticsDay { get; set; }
+        public DbSet<StatisticsRawMaterial> StatisticsRawMaterial { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<ValidationTestDay> ValidationTestDay { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.ApplyConfiguration(new DescriptionRawMaterialMapping());
+            modelBuilder.ApplyConfiguration(new EstablishmentMapping());
+            modelBuilder.ApplyConfiguration(new HistoryRawMaterialMapping());
+            modelBuilder.ApplyConfiguration(new RawMaterialMapping());
+            modelBuilder.ApplyConfiguration(new SaleDayMapping());
+            modelBuilder.ApplyConfiguration(new StatisticsDayMapping());
+            modelBuilder.ApplyConfiguration(new StatisticsRawMaterialMapping());
+            modelBuilder.ApplyConfiguration(new UserMapping());
+            modelBuilder.ApplyConfiguration(new ValidationTestDayMapping());
+
             base.OnModelCreating(modelBuilder);
         }
     }
